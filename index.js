eae042dae2d9d27484916be1f5bf386db8499783
Success Chidinma Ongharaka <89156476+Success-O@users.noreply.github.com>
2021-09-07 11:40:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,57 @@
     form.lastName.focus();
     return;
   }
+  if(form.dateOfBirth.value=="")
+  {
+    alert("Enter your Date of birth!");
+    form.dateOfBirth.focus();
+    return;
+  }
+  if(form.placeOfBirth.value=="")
+  {
+    alert("Enter your Place of birth!");
+    form.placeOfBirth.focus();
+    return;
+  }
+  if(form.stateOfOrigin.value=="")
+  {
+    alert("Enter State of origin!");
+    form.stateOfOrigin.focus();
+    return;
+  }
+  if(form.residentialArea.value=="")
+  {
+    alert("Fill up this field!");
+    form.residentialArea.focus();
+    return;
+  }
+  if(form.LGA.value=="")
+  {
+    alert("Fill up this field!");
+    form.LGA.focus();
+    return;
+  }
+  if(form.city.value=="")
+  {
+    alert("Fill up this field")
+    form.city.focus();
+    return;
+  }
+  if(form.country.value=="")
+  {
+    alert("Fill up this field!");
+    form.country.focus();
+    return;
+  }
+  if(form.phoneNumber.value=="")
+  {
+    alert("Fill up this field!")
+    form.phoneNumber.focus();
+    return;
+  }
+  if((formValidate.sex[0].checked==false)&&(formValidate.sex[1].checked==false))
+  {
+    alert("Choose the Gender!")
+    return false;
+  }
+}
